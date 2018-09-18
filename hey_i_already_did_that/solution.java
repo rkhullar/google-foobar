b5ebd245f0c27c6845b295083321258ae897655b
Rajan Khullar <rkhullar03@gmail.com>
2018-09-18 09:18:57 -0400

    starting third problem

---
+++ hey_i_already_did_that/solution.java
@@ -0,0 +1,9 @@
+package​ ​com.google.challenges;​ ​
+
+public​ ​class​ ​Answer​ ​{​ ​​ ​​ ​
+​ ​​ ​​ ​​ ​public​ ​static​ ​int​ ​answer(String​ ​n,​ ​int​ ​b)​ ​{​ ​
+
+​ ​​ ​​ ​​ ​​ ​​ ​​ ​​ ​//​ ​Your​ ​code​ ​goes​ ​here.
+
+​ ​​ ​​ ​​ ​}​ ​
+}
