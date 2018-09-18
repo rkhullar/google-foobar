d7c0b1e4221e9be27be3eb0c2621bb2a6e7cde43
Rajan Khullar <rkhullar03@gmail.com>
2018-09-18 17:14:23 -0400

    more tests

--- hey_i_already_did_that/solution.py
+++ hey_i_already_did_that/solution.py
@@ -1,12 +1,36 @@
-from typing import Iterator
+from typing import Union, List, Iterator, Type
+
+Number = Union[str, List[int]]
 
 
-def difference(x, y, base=10, digits=4):
-    # type: (str, str, int, int) -> str
+def number_str_to_int_array(number):
+    # type: (Number) -> Number
+    if isinstance(number, str):
+        return list(map(int, number))
+    elif isinstance(number, list):
+        return number
+
+
+def return_number(number, return_type=str):
+    # type: (Number, Type) -> Number
+    array = number_str_to_int_array(number)
+    if return_type is str:
+        return ''.join(map(str, array))
+    elif return_type is list:
+        return array
+
+
+def difference(x, y, base=10, digits=4, return_type=str):
+    # type: (Number, Number, int, int, Type) -> Number
     """
     determine difference of two numbers in a particular base
     """
-    return ''
+    complement = base - 1
+    X, Y = map(number_str_to_int_array, [x, y])
+
+    Y_prime = [complement - value for value in Y]
+
+    return return_number(Y_prime, return_type=return_type)
 
 
 def generator(seed, base):
@@ -38,3 +62,11 @@ def answer(n, b):
     determine size of cyclic group given start and base
     """
     pass
+
+
+if __name__ == '__main__':
+    import sys
+    print(sys.version)
+
+    r = difference('654', '123', base=10, digits=3, return_type=list)
+    print r
--- hey_i_already_did_that/tests.py
+++ hey_i_already_did_that/tests.py
@@ -4,6 +4,25 @@ import unittest
 
 class SolutionTest(unittest.TestCase):
 
+    def test_number_str_to_int_array(self):
+        x, e = '1234', [1, 2, 3, 4]
+        y = number_str_to_int_array(x)
+        self.assertEqual(e, y)
+        y = number_str_to_int_array(y)
+        self.assertEqual(e, y)
+
+    def test_return_number_as_list(self):
+        x = '1234'
+        e = [1, 2, 3, 4]
+        y = return_number(x, return_type=list)
+        self.assertEqual(e, y)
+
+    def test_return_number_as_str(self):
+        x = [1, 2, 3, 4]
+        e = '1234'
+        y = return_number(x, return_type=str)
+        self.assertEqual(e, y)
+
     def test_difference_1(self):
         result = difference('4000', '1111', base=10, digits=4)
         self.assertEqual('2999', result)
