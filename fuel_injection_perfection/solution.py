10eed0006725f5c70925f9a1bb91f57e9bdb41af
Rajan Khullar <rkhullar03@gmail.com>
2018-10-13 11:57:41 -0400

    adding solution

--- fuel_injection_perfection/solution.py
+++ fuel_injection_perfection/solution.py
@@ -1,3 +1,28 @@
+from typing import Iterator
+
+
+def reduction(n):
+    # type: (int) -> Iterator[int]
+    """
+    reduction sequence from n to 1 by either adding 1, subtracting 1, or dividing by 2
+    """
+    yield n
+    x = n
+    if x in {1, 2}:
+        if x == 2:
+            yield 1
+        return
+    while x > 1:
+        r = x % 4
+        a = [x/2, x-1, x/2, x+1]
+        x = a[r]
+        yield x
+
+
 def answer(n):
-    # your code here
-    pass
+    # type: (str) -> int
+    """
+    minimum number of operations to reduce n to 1 by either adding 1, subtracting 1, or dividing by 2
+    """
+    sequence = reduction(int(n))
+    return sum(1 for _ in sequence) - 1
--- fuel_injection_perfection/tests.py
+++ fuel_injection_perfection/tests.py
@@ -1,6 +1,39 @@
+from parameterized import parameterized
+from solution import *
 import unittest
 
 
+class ReductionTest(unittest.TestCase):
+
+    @parameterized.expand([
+        [1, [1]],
+        [2, [2, 1]],
+        [4, [4, 2, 1]],
+        [15, [15, 16, 8, 4, 2, 1]]
+    ])
+    def test(self, x, e):
+        y = list(reduction(x))
+        self.assertEqual(e, y)
+
+
 class SolutionTest(unittest.TestCase):
-    def test_1(self):
-        pass
+
+    @parameterized.expand([
+        ['4', 2],
+        ['15', 5]
+    ])
+    def test_examples(self, x, e):
+        y = answer(x)
+        self.assertEqual(e, y)
+
+    def test_309_digits(self):
+        x = '9' * 309
+        i = int(x)
+        self.assertEqual(x, str(i))
+        y = answer(x)
+
+    def test_600_digits(self):
+        x = '9' * 600
+        i = int(x)
+        self.assertEqual(x, str(i))
+        y = answer(x)
